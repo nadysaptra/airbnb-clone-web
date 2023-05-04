d704c38448d2d1900c6a61d10c9bfdd76037d43c
nadysaptra <saputranady@gmail.com>
2023-05-04 16:53:47 +0700

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { Nunito } from 'next/font/google'
+const font = Nunito({
+  subsets: ["latin"]
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+      <style dangerouslySetInnerHTML={{ __html: `
+        :root {
+          --font-sans: ${font.style.fontFamily};
+        }`}} />
+     <Component {...pageProps} />
+    </>
 }
